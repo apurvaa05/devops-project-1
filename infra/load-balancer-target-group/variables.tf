4eabf5d1879b4f01d26309a902970f7bf50de3e1
apurvaa05 <167891534+apurvaa05@users.noreply.github.com>
2025-05-29 06:42:12 +0530

    Update variables.tf

--- infra/load-balancer-target-group/variables.tf
+++ infra/load-balancer-target-group/variables.tf
@@ -1,19 +1,1 @@
-variable "lb_target_group_name" {
-  type = string
-}
 
-variable "lb_target_group_port" {
-  type = number
-}
-
-variable "lb_target_group_protocol" {
-  type = string
-}
-
-variable "vpc_id" {
-  type = string
-}
-
-variable "ec2_instance_id" {
-  type = string
-}
